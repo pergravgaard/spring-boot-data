7b497e297ffd9ab4ca0def19da57d28112279a12
pgr <per.gravgaard@gmail.com>
2018-10-06 18:21:50 +0200

    Normal config

--- src/test/integration/java/com/company/config/TestConfig.java
+++ src/test/integration/java/com/company/config/TestConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.PropertySources;
 
 @ComponentScan("com.company")
 @PropertySources({
-        @PropertySource(value = {"classpath:mariadb-test.properties"})
-//        @PropertySource(value = {"classpath:h2.properties", "classpath:h2-${env}.properties"})
+//        @PropertySource(value = {"classpath:mariadb-test.properties"})
+        @PropertySource(value = {"classpath:h2-test.properties"})
 })
 @Import({JpaDataSourceConfig.class, JpaConfig.class})
 public class TestConfig {
