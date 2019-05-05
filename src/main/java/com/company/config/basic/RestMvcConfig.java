7989cc93aee315ffcb93b99ea591d3ef8adc2f97
pgr <per.gravgaard@gmail.com>
2019-05-05 18:28:07 +0200

    Turned out that the configuration code wasn't obsolete after all

--- src/main/java/com/company/config/basic/RestMvcConfig.java
+++ src/main/java/com/company/config/basic/RestMvcConfig.java
@@ -1,8 +1,5 @@
 package com.company.config.basic;
 
-import com.company.formatter.BaseDateFormatter;
-import com.company.formatter.BaseDateTimeFormatter;
-import com.company.formatter.ZonedDateTimeFormatter;
 import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,29 +7,48 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.ConfigurableConversionService;
 import org.springframework.core.env.Environment;
+import org.springframework.data.auditing.AuditableBeanWrapper;
+import org.springframework.data.auditing.AuditableBeanWrapperFactory;
+import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.data.mapping.PersistentEntity;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.HttpHeadersPreparer;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.format.FormatterRegistry;
+import org.springframework.data.rest.webmvc.support.ETag;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
 
+import java.time.Instant;
+import java.util.Optional;
+
+/**
+ * Solves 1 issue:
+ * - The lack of a converter from java.util.Date to ZonedDateTime - simply by not converting to Date
+ */
 public class RestMvcConfig extends RepositoryRestMvcConfiguration {
 
     @Autowired
     private Environment env;
 
+    private final ObjectFactory<ConversionService> conversionServiceObjectFactory;
     private RepositoryRestConfiguration repositoryRestConfiguration;
 
     public RestMvcConfig(ApplicationContext context, ObjectFactory<ConversionService> conversionServiceObjectFactory) {
         super(context, conversionServiceObjectFactory);
+        this.conversionServiceObjectFactory = conversionServiceObjectFactory;
     }
 
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        super.addFormatters(registry);
-        registry.addFormatter(new BaseDateTimeFormatter());
-        registry.addFormatter(new BaseDateFormatter());
-        registry.addFormatter(new ZonedDateTimeFormatter());
-    }
+//    @Override
+//    public void addFormatters(FormatterRegistry registry) {
+//        super.addFormatters(registry);
+//        registry.addFormatter(new BaseDateTimeFormatter());
+//        registry.addFormatter(new BaseDateFormatter());
+//        registry.addFormatter(new ZonedDateTimeFormatter());
+//    }
 
     /**
      * Main configuration for the REST exporter.
@@ -48,9 +64,90 @@ public class RestMvcConfig extends RepositoryRestMvcConfiguration {
         return repositoryRestConfiguration;
     }
 
+    @Bean
+    @Override
+    public HttpHeadersPreparer httpHeadersPreparer() {
+        return new MyHttpHeadersPreparer(auditableBeanWrapperFactory());
+    }
+
     @JsonIgnoreType
     private static class MixinClass {
         // empty class for json mapping
+    }
+
+    public class MyHttpHeadersPreparer extends HttpHeadersPreparer {
+
+        private final @NonNull AuditableBeanWrapperFactory auditableBeanWrapperFactory;
+
+        MyHttpHeadersPreparer(AuditableBeanWrapperFactory auditableBeanWrapperFactory) {
+            super(auditableBeanWrapperFactory);
+            this.auditableBeanWrapperFactory = auditableBeanWrapperFactory;
+            ConfigurableConversionService conversionService = (ConfigurableConversionService) conversionServiceObjectFactory.getObject();
+            Jsr310Converters.getConvertersToRegister().forEach(conversionService::addConverter);
+        }
+
+
+        /**
+         * Returns the default headers to be returned for the given {@link PersistentEntityResource}. Will set {@link ETag}
+         * and {@code Last-Modified} headers if applicable.
+         *
+         * @param resource can be {@literal null}.
+         * @return
+         */
+        public HttpHeaders prepareHeaders(Optional<PersistentEntityResource> resource) {
+            return resource.map(it -> prepareHeaders(it.getPersistentEntity(), it.getContent())).orElseGet(HttpHeaders::new);
+        }
+
+        /**
+         * Returns the default headers to be returned for the given {@link PersistentEntity} and value. Will set {@link ETag}
+         * and {@code Last-Modified} headers if applicable.
+         *
+         * @param entity must not be {@literal null}.
+         * @param value must not be {@literal null}.
+         * @return
+         */
+        public HttpHeaders prepareHeaders(PersistentEntity<?, ?> entity, Object value) {
+
+            Assert.notNull(entity, "PersistentEntity must not be null!");
+            Assert.notNull(value, "Entity value must not be null!");
+
+            // Add ETag
+            HttpHeaders headers = ETag.from(entity, value).addTo(new HttpHeaders());
+
+            // Add Last-Modified
+            getLastModifiedInMilliseconds(value).ifPresent(headers::setLastModified);
+
+            return headers;
+        }
+
+        public boolean isObjectStillValid(Object source, HttpHeaders headers) {
+
+            Assert.notNull(source, "Source object must not be null!");
+            Assert.notNull(headers, "HttpHeaders must not be null!");
+
+            if (headers.getIfModifiedSince() == -1) {
+                return false;
+            }
+
+            return getLastModifiedInMilliseconds(source)//
+                    .map(it -> it / 1000 * 1000 <= headers.getIfModifiedSince())//
+                    .orElse(true);
+        }
+
+
+        private Optional<Long> getLastModifiedInMilliseconds(Object object) {
+
+            return auditableBeanWrapperFactory.getBeanWrapperFor(object)
+//                    .flatMap(auditableBeanWrapper -> {
+//                        System.out.println(auditableBeanWrapper);
+//                        return auditableBeanWrapper.getLastModifiedDate();
+//                    })
+                    .flatMap(AuditableBeanWrapper::getLastModifiedDate)
+                    //.map(it -> conversionService.convert(it, Date.class)) // this line is obsolete and actually causes problems
+                    .map(it -> conversionServiceObjectFactory.getObject().convert(it, Instant.class))
+                    .map(Instant::toEpochMilli);
+        }
+
     }
 
 }
